61460f60e409581c2f9de7291c0b2c3ef19f8af0
Matt Dusig <mattdusig@icloud.com>
2025-07-25 16:10:46 -0700

    fix: Defer client init in final two pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,8 @@
 import { LoginForm } from '@/components/auth/login-form';
 
+// Prevent this page from being built statically
+export const dynamic = 'force-dynamic';
+
 const LoginPage = () => {
   return <LoginForm />;
 };
